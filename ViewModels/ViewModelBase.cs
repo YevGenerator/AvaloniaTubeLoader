dd757fb93e8069e7970e9b56e1599455e1d59793
Yevgenerator <percev.yevgen@gmail.com>
2024-08-05 18:43:49 +0300

    tube

---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using SharpTube;
+using VideoLibrary;
+
+namespace AvaloniaTubeLoader.ViewModels;
+
+public partial class ViewModelBase : ObservableObject, IVideoContainer<ObservableCollection<YouTubeVideo>>
+{
+    [ObservableProperty]
+    private string _videoName = "";
+
+    public YouTubeVideo? MuxedVideo { get; set; }
+    public ObservableCollection<YouTubeVideo> Videos { get; set; } = [];
+    public ObservableCollection<YouTubeVideo> Audios { get; set; } = [];
+
+    public void Test()
+    {
+        var runner = new YoutubeRunner();
+        YoutubeRunner.DistributeVideos(runner.GetVideos(""), this );
+    }
+    
+}
